7eb4e69cc19e402aeefeb2752e82df979d03c6c3
Sasha Estrella <sestrella@intranet.certant.com>
2021-09-01 20:59:39 -0300

    se agregan animaciones y demás

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+
+  lastScrollTop = 0;
+
+  @HostListener("window:scroll", ['$event'])
+  doSomethingOnWindowsScroll($event:Event){
+    let scrollOffset = $event.srcElement;
+    console.log("window scroll: ", scrollOffset);
+  }
 
   ngOnInit(): void {
   }
